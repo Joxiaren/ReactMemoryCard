aedbd25b6cc770fbef8b69394df0212bd259b4cc
Joxiaren <2000jox@gmail.com>
2024-02-21 23:15:17 +0100

    add working game

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,7 +5,7 @@ import './card.css';
 function Card({ name, imageFront, imageBack, onclick}){
 
     return (
-        <div className="card-container" onClick={onclick}>
+        <div className="card-container" onClick={()=>onclick(name)}>
             <div className="card">
                 <img className="img-front" src={imageFront}/>
                 <img className="img-back" src={imageBack}/>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,11 +6,13 @@ import config from '../../../conifg.json';
 import Card from '../Card/Card';
 import shuffleArray from '../../scripts/shuffle';
 
-
+let selectedCards = {};
+let winCondition = false;
 function Main({ gameName }){
 
     const [cards, setCards] = useState([]);
-
+    const [score, setScore] = useState(0);
+    
     useEffect(() => {
         console.log(`gameName ${gameName}`);
         fetch(`${config.localApiAddress}/bossInfo/${gameName}`)
@@ -25,17 +27,55 @@ function Main({ gameName }){
         }
     }, [gameName])
 
-    let cardClick = (e) => {
+    let cardClick = (card) => {
         console.log('clicked');
-        console.log(cards);
         //animation
-        setCards(shuffleCards([...cards]));
-    }   
+        if(card in selectedCards){
+            selectedCards = {};
+        }
+        else{
+            selectedCards[card] = "selected";
+        }
 
+        let scoreCalculate = Object.keys(selectedCards).length;
+        let newCards = shuffleCards([...cards]);
+        
+        if(scoreCalculate >= newCards.length){
+            winCondition = true;
+        } 
+        else{
+            let fair = false;
+            for(let j = 0; j < 10; j++){
+                if(!(newCards[j].name in selectedCards)){
+                    fair = true;
+                    break;
+                }
+            }
+    
+            if(!fair){
+                console.log("mroao sam aaaaa");
+                let i = 10;
+                while(newCards[i].name in selectedCards) i++;
+                [newCards[0], newCards[i]] = [newCards[i], newCards[0]];
+                let first10 = newCards.slice(0, 10);
+                first10 = shuffleCards(first10);
+                first10.map((elem, index) => newCards[index] = elem);
+            }
+        }
+        setCards(newCards);
+        setScore(scoreCalculate);
+    }   
+    
+    if(winCondition) return (
+        <main>
+            <div className='win'>VICTORY ACHIEVED</div>
+        </main>
+    );
     return (
         <main>
+            <div className='scoreboard'>Score: {score}</div>
             {
-            cards.map((card) => (
+            cards.slice(0,10).map((card) => (
                 <Card key={card.name} name={card.name} imageFront={`${config.localApiAddress}/${card.image}`} imageBack={`/covers/${gameName}.jpg`} onclick={cardClick}/>
             ))}
         </main>
